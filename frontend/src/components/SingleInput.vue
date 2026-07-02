d5555e1f6b335dc1bf87b30ce673ea0d4ea2a66b
ebedi <ituzeebedi12@gmail.com>
2026-07-02 22:17:26 +0200

    refactor[single input ]:refacto design of color on single input   componetns

--- frontend/src/components/SingleInput.vue
+++ frontend/src/components/SingleInput.vue
@@ -23,13 +23,13 @@
   }
 </script>
 <template>
-  <p v-if="props.requestVar" class="text-gray-100">Insert {{ props.defs.name }} <b>{{ props.requestVar }}</b></p>
-  <form class="flex gap-4 flex-wrap" @submit.prevent="insertValue">
-    <input v-model="inputValue" :type="props.defs.type" :name="props.defs.name" :placeholder="props.defs.placeholder" class="p-2 focus:outline-none dark:bg-gray-800 dark:text-gray-200 rounded-full" autocomplete="off" />
-    <button class="bg-emerald-600 text-white px-6 py-2 rounded-full w-fit font-semibold">
+  <p v-if="props.requestVar" class="text-[#171717] dark:text-[#ffffff] text-sm font-medium">Insert {{ props.defs.name }} <b>{{ props.requestVar }}</b></p>
+  <form class="flex gap-3 flex-wrap" @submit.prevent="insertValue">
+    <input v-model="inputValue" :type="props.defs.type" :name="props.defs.name" :placeholder="props.defs.placeholder" class="px-3 py-2 text-sm bg-[#ffffff] dark:bg-[#000000] border border-neutral-200 dark:border-neutral-700 rounded-md text-[#171717] dark:text-[#ffffff] focus:outline-none focus:ring-1 focus:ring-neutral-400 outline-none transition-colors" autocomplete="off" />
+    <button class="px-4 py-2 bg-[#171717] text-[#ffffff] dark:bg-[#ffffff] dark:text-[#000000] hover:bg-neutral-800 dark:hover:bg-neutral-100 font-semibold text-sm rounded-md transition-colors w-fit">
       Insert
     </button>
-    <button @click="props.defs.show = false" class="bg-red-600 text-white px-6 py-2 rounded-full w-fit font-semibold">
+    <button type="button" @click="props.defs.show = false" class="px-4 py-2 border border-neutral-200 dark:border-neutral-800 bg-transparent text-[#171717] dark:text-[#ffffff] hover:bg-neutral-50 dark:hover:bg-neutral-900 font-semibold text-sm rounded-md transition-colors w-fit">
       Cancel
     </button>
   </form>
